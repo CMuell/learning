649cb5e23d040680cbb6c53482e8374249c9d2d2
CMuell <35719313+CMuell@users.noreply.github.com>
2023-04-09 20:59:12 -0700

    1 file modified

--- index.js
+++ index.js
@@ -50,24 +50,16 @@ first row starts with ` `
 */
 
 function checkerboard() {
-  let board = ``;
-
-  //for (let row = 0; row <=)
-
-  for (let n = 1; n <= 8; n++) {
-    switch (n % 2 == 0 ? true : false) {
-      case true:
-        board += ` `;
-        break;
-      case false:
-        board += `#`;
-        break;
-    }
+  let board = '',
+    pattern = '';
+  function writeRow(current, goal) {}
+  function even(n) {
+    pattern = ' #';
   }
-  return board;
+  function odd(n) {
+    pattern = '# ';
+  }
 }
-
-console.log(checkerboard());
 
 /*
 
@@ -75,29 +67,44 @@ if `row` is even, the first character in the string is ` `.
 
 */
 
-// target = number to reach
-function findSolution(target) {
-  function find(current, history) {
-    // if binding `current` is equal to `target`, the solution has been reached
-    // and we will return `history` which is a string containing the steps used to reach the target
-    if (current == target) {
-      return history;
-    }
-    // if binding `current` is larger than `target`, we will return `null` since
-    // any further operation will also be greater than `target`
-    else if (current > target) {
-      return null;
-    }
-    // finally, if the binding `current` is less than `target`, we will call this function from
-    // within itself.
-    else {
-      return (
-        find(current + 5, `(${history} + 5)`) ||
-        find(current * 3, `(${history} * 3)`)
-      );
-    }
-  }
-  return find(1, '1');
-}
+function execute(num) {
+  console.log(findSolution(num));
+  // target = number to reach
+  function findSolution(target) {
+    function find(current, history) {
+      // if binding `current` is equal to `target`, the solution has been reached
+      // and we will return `history` which is a string containing the steps used to reach the target
+      if (current == target) {
+        return history;
+      }
+      // if binding `current` is larger than `target`, we will return `null` since
+      // any further operation will also be greater than `target`
+      else if (current > target) {
+        return null;
+      }
+      // finally, if the binding `current` is less than `target`, we will call this function from
+      // within itself.
+      else {
+        return (
+          /*
+          these two function calls are equal to:
+            find(1 + 5, `(1 + 5)`) || find(1 * 3, `(1 * 3)`)
 
-console.log(findSolution(48));
+            then this local binding, if current (6) is still less than target (24), 
+            would again call find() in the `else` block.
+
+            this time the call would look like:
+              find(6 + 5, `((1 + 5) + 5)`) || find(6 * 3, `((1 * 3) * 3)`)
+
+            and so on, until either `current` == `target`, or `current` > `target`. 
+            Only current matching target will return the history.
+        */
+          find(current + 5, `(${history} + 5)`) ||
+          find(current * 3, `(${history} * 3)`)
+        );
+      }
+    }
+    // First call of find()
+    return find(1, '1');
+  }
+}
